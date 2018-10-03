8915223285fa35b13ffab8a5997c7d726a1095b0
Jordan Leeper <jordan.leeper@visionistinc.com>
2018-10-03 10:55:20 -0400

    switch to yarn, added css modules

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import styles from './App.css';
+
+const App = () => (
+  <div className={styles.app}>
+    <Header />
+    <div className={styles.body}> Content Example </div>
+  </div>
+);
+
+export default App;
